aef21f8df434dbdf4c0ee039e469601224286a90
Juan Vanegas <juan.vanegas@loka.com>
2024-11-30 11:21:12 -0500

    :sparkles: Create minimal Euler integrator

--- integrator/main.c
+++ integrator/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+float step(float time, float velocity, float acceleration) {
+    return velocity * time + 0.5 * acceleration * time * time;
+}
+
+float *integrate(float v0, float a, int steps) {
+    float *distance = malloc(sizeof(float) * steps);
+
+    float dt = 0.01;
+    for (int i = 0; i < steps; i++) {
+        distance[i] = step(i * dt, v0, a);
+    }
+    return distance;
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int steps = 200;
+    float *distance = integrate(10, -9.8, steps);
+
+    for (int i = 0; i < steps; i++) {
+        printf("%d,%f\n", i, distance[i]);
+    }
+
     return 0;
 }
